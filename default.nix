3d422a7a9d6c8af561f0bf289a80b2ca78b53e37
Juan Ibiapina <juanibiapina@gmail.com>
2022-06-08 14:54:17 +0200

    Add function to generate ruby package names from .ruby-version

--- default.nix
+++ default.nix
@@ -6,7 +6,16 @@ let
     inherit (super.darwin.apple_sdk.frameworks) Foundation;
   });
   rubyVersion = import ./ruby-version.nix { inherit (super) lib; };
+
+  parseRubyVersionFile = import ./parse-ruby-version-file.nix;
+
+  pwd = builtins.getEnv "PWD";
+  ruby_version_string = builtins.readFile (pwd + "/.ruby-version");
+  ruby_version_parts = builtins.match "([0-9]*)\.([0-9]*)\.([0-9]*).*" ruby_version_string;
+  ruby_package = builtins.concatStringsSep "_" (["ruby"] ++ ruby_version_parts);
 in {
+  parseRubyVersionFile = parseRubyVersionFile;
+
   ruby_2_7_6 = rubyGenericBuilder {
     version = rubyVersion "2" "7" "6" "";
     sha256 = "042xrdk7hsv4072bayz3f8ffqh61i8zlhvck10nfshllq063n877";
