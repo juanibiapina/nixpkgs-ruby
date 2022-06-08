b9d4bcae74f409c2a7ee316cdb8a03b074bc27d8
Juan Ibiapina <juanibiapina@gmail.com>
2022-06-08 14:46:58 +0200

    Extract ruby generic builder to its own file

--- default.nix
+++ default.nix
@@ -1,10 +1,19 @@
 self: super:
 
-{
-  inherit (super.callPackage ./ruby.nix {
+let
+  rubyGenericBuilder = (super.callPackage ./generic-builder.nix {
     inherit (super.darwin) libiconv libobjc libunwind;
     inherit (super.darwin.apple_sdk.frameworks) Foundation;
-  })
-    ruby_2_7_6
-    ruby_3_1_2;
+  });
+  rubyVersion = import ./ruby-version.nix { inherit (super) lib; };
+in {
+  ruby_2_7_6 = rubyGenericBuilder {
+    version = rubyVersion "2" "7" "6" "";
+    sha256 = "042xrdk7hsv4072bayz3f8ffqh61i8zlhvck10nfshllq063n877";
+  };
+
+  ruby_3_1_2 = rubyGenericBuilder {
+    version = rubyVersion "3" "1" "2" "";
+    sha256 = "0gm84ipk6mrfw94852w5h7xxk2lqrxjbnlwb88svf0lz70933131";
+  };
 }
--- generic-builder.nix
+++ generic-builder.nix
@@ -14,9 +14,6 @@ let
   config = import ./config.nix { inherit fetchFromSavannah; };
   rubygems = import ./rubygems { inherit stdenv lib fetchurl; };
 
-  # Contains the ruby version heuristics
-  rubyVersion = import ./ruby-version.nix { inherit lib; };
-
   generic = { version, sha256 }: let
     ver = version;
     tag = ver.gitTag;
@@ -255,14 +252,5 @@ let
       }
     ) args; in self;
 
-in {
-  ruby_2_7_6 = generic {
-    version = rubyVersion "2" "7" "6" "";
-    sha256 = "042xrdk7hsv4072bayz3f8ffqh61i8zlhvck10nfshllq063n877";
-  };
-
-  ruby_3_1_2 = generic {
-    version = rubyVersion "3" "1" "2" "";
-    sha256 = "0gm84ipk6mrfw94852w5h7xxk2lqrxjbnlwb88svf0lz70933131";
-  };
-}
+in
+  generic
