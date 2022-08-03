04f79063f40fb5a7d93554a1e4293db353239ec1
Juan Ibiapina <juanibiapina@gmail.com>
2022-08-03 13:41:53 +0200

    Use mkRuby from nixpkgs

--- default.nix
+++ default.nix
@@ -1,23 +1,17 @@
 self: super:
 
 let
-  rubyGenericBuilder = (super.callPackage ./generic-builder.nix {
-    inherit (super.darwin) libiconv libobjc libunwind;
-    inherit (super.darwin.apple_sdk.frameworks) Foundation;
-  });
-  rubyVersion = import ./ruby-version.nix { inherit (super) lib; };
-
   parseRubyVersionFile = import ./parse-ruby-version-file.nix;
 in {
   inherit parseRubyVersionFile;
 
-  ruby_2_7_6 = rubyGenericBuilder {
-    version = rubyVersion "2" "7" "6" "";
+  ruby_2_7_6 = super.mkRuby {
+    version = super.mkRubyVersion "2" "7" "6" "";
     sha256 = "042xrdk7hsv4072bayz3f8ffqh61i8zlhvck10nfshllq063n877";
   };
 
-  ruby_3_1_2 = rubyGenericBuilder {
-    version = rubyVersion "3" "1" "2" "";
+  ruby_3_1_2 = super.mkRuby {
+    version = super.mkRubyVersion "3" "1" "2" "";
     sha256 = "0gm84ipk6mrfw94852w5h7xxk2lqrxjbnlwb88svf0lz70933131";
   };
 }
