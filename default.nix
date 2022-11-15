40218d3d73850b13926435763077cdb5a607dbf8
Juan Ibiapina <juanibiapina@gmail.com>
2022-11-15 14:35:26 +0100

    Add support for ruby 3.0.4

--- default.nix
+++ default.nix
@@ -10,6 +10,11 @@ in {
     sha256 = "042xrdk7hsv4072bayz3f8ffqh61i8zlhvck10nfshllq063n877";
   };
 
+  ruby_3_0_4 = super.mkRuby {
+    version = super.mkRubyVersion "3" "0" "4" "";
+    sha256 = "0avj4g3s2839b2y4m6pk8kid74r8nj7k0qm2rsdcwjzhg8h7rd3h";
+  };
+
   ruby_3_1_2 = super.mkRuby {
     version = super.mkRubyVersion "3" "1" "2" "";
     sha256 = "0gm84ipk6mrfw94852w5h7xxk2lqrxjbnlwb88svf0lz70933131";
--- test.nix
+++ test.nix
@@ -6,6 +6,7 @@
 {
   inherit (pkgs)
     ruby_2_7_6
+    ruby_3_0_4
     ruby_3_1_2
   ;
 }
